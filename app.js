81f2a459fba6fec51ed459c91453aeea6bd90ddf
diwaker1apr <77387915+diwaker1apr@users.noreply.github.com>
2024-11-17 09:02:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello! This is tutorial'));
+app.get('/', (req, res) => res.send('Hello! the code has been changed second time'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
